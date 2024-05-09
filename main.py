cfcf57644bc45f34563d4b4bddad8d7020af11fb
Nobody <mishasikolenko@mail.ru>
2024-05-09 18:05:53 +0300

    Code clean

--- main.py
+++ main.py
@@ -111,7 +111,7 @@ def show_users_data(user_id : int) -> None:
         case 0:
             response = f"{user_data[5]}, here is the information you provided: \n\tGender: {user_data[2]};\n\tHeight: {user_data[3]};\n\tWeight: {user_data[4]};\n\tPurpose of using the bot: \"{user_data[6]}\";\n\tDiet: \"{user_data[9]}\"."
         case 1: 
-            response = f"{user_data[5]}, вот информация, которую вы предоставили: \n\tПол: {user_data[2]};\n\tРост: {user_data[3]};\n\tВес:{user_data[4]};\n\tЦель использования бота: \"{user_data[6]}\";\n\tДиета: \"{user_data[9]}\"."
+            response = f"{user_data[5]}, вот информация, которую вы предоставили: \n\tПол: {user_data[2]};\n\tРост: {user_data[3]};\n\tВес: {user_data[4]};\n\tЦель использования бота: \"{user_data[6]}\";\n\tДиета: \"{user_data[9]}\"."
         case 2:
             response = f"{user_data[5]}, вось інфармацыя, якую вы пазначылі: \n\tПол: {user_data[2]};\n\tРост: {user_data[3]};\n\tВас: {user_data[4]};\n\tМэта выкарыстанне бота: \"{user_data[6]}\";\n\tДыета: \"{user_data[9]}\"."
     
