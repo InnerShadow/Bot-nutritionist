1223829f20ff0094a12d68df61b33a8d88233b7a
Nobody <mishasikolenko@mail.ru>
2024-05-11 14:38:57 +0300

    Add skip button

--- main.py
+++ main.py
@@ -36,12 +36,15 @@ def choose_gender(user_id : int) -> None:
         case 0:
             male_text = "Male"
             female_text = 'Female'
+            skip_text = "Skip all questions"
         case 1: 
             male_text = "Мужчина"
             female_text = 'Женщина'
+            skip_text = "Пропустить все вопросы"
         case 2:
             male_text = "Мыжчына"
             female_text = 'Жанчына'
+            skip_text = "Прапусціць усе пытанні"
 
     match get_language(user_id):
         case 0:
@@ -51,9 +54,12 @@ def choose_gender(user_id : int) -> None:
         case 2:
             response = "Калі ласка, пазнацче Ваш пол:"
     markup = types.InlineKeyboardMarkup()
-    markup.add(types.InlineKeyboardButton(male_text, callback_data='Male'), 
-               types.InlineKeyboardButton(female_text, callback_data='Female'))
-    bot.send_message(user_id, response, reply_markup=markup)
+    btn1 = types.InlineKeyboardButton(male_text, callback_data = 'Male')
+    btn2 = types.InlineKeyboardButton(female_text, callback_data = 'Female')
+    btn3 = types.InlineKeyboardButton(skip_text, callback_data = 'Skip')
+    markup.row(btn1, btn2)
+    markup.row(btn3)
+    bot.send_message(user_id, response, reply_markup = markup)
     states[user_id] = "choose_gender"
 
 
@@ -134,6 +140,17 @@ def handle_callback_query(call : telebot.types.Message) -> None:
     user_id = call.from_user.id
     if user_id not in states:
         start_dialog(call.message)
+    elif call.data == "Skip":
+         states[user_id] = 0
+         match get_language(user_id):
+            case 0:
+                response = "I am ready to answer any of your questions!!!"
+            case 1: 
+                response = "Я готов ответить на любые Ваши вопросы!!!"
+            case 2:
+                response = "Я гатовы адказаць на любыя вашы пытанні!!!"
+    
+         bot.send_message(user_id, response)
     elif states[user_id] == "start_dialog":
         chosen_language = call.data
         update_user_language(user_id, chosen_language)
@@ -225,6 +242,7 @@ def handle_text(message : telebot.types.Message) -> None:
 
 def photo_response(message : telebot.types.Message) -> None:
     user_id = message.from_user.id
+    states[user_id] = 0
     if check_chat_existance(user_id):
         if user_id not in states:
             states[user_id] = 0
@@ -259,6 +277,7 @@ def photo_response(message : telebot.types.Message) -> None:
 
 def voice_response(message : telebot.types.Message) -> None:
     user_id = message.from_user.id
+    states[user_id] = 0
     if check_chat_existance(user_id):
         if user_id not in states:
             states[user_id] = 0
